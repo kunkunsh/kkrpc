6648d2ee57aab49e105e0b568bcb203fb94f2d44
Huakun Shen <huakun.shen@huakunshen.com>
2024-11-17 06:47:01 -0500

    ci: bypass deno error in cloudflare pages

--- packages/kkrpc/scripts/prepare.ts
+++ packages/kkrpc/scripts/prepare.ts
@@ -1,5 +1,11 @@
 import { $ } from "bun"
 
+// check if deno is installed
+const deno = await $`deno`.text()
+if (!deno) {
+	process.exit(0)
+}
+
 const denoTypes = await $`deno types`.text()
 // filter out the line with no-default-lib
 const denoTypesFiltered = denoTypes.split("\n").filter((line) => !line.includes("no-default-lib"))
