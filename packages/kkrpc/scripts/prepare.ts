b23ff1ce4583715610535984a39b3d344952ac59
Huakun Shen <huakun.shen@huakunshen.com>
2024-11-17 02:41:40 -0500

    feat: add husky git hook

--- packages/kkrpc/scripts/prepare.ts
+++ packages/kkrpc/scripts/prepare.ts
@@ -1,6 +1,6 @@
 import { $ } from "bun"
 
-const denoTypes = await $`deno types`.text()
-// filter out the line with no-default-lib
-const denoTypesFiltered = denoTypes.split("\n").filter((line) => !line.includes("no-default-lib"))
-await Bun.write("./deno.d.ts", denoTypesFiltered.join("\n"))
+// const denoTypes = await $`deno types`.text()
+// // filter out the line with no-default-lib
+// const denoTypesFiltered = denoTypes.split("\n").filter((line) => !line.includes("no-default-lib"))
+// await Bun.write("./deno.d.ts", denoTypesFiltered.join("\n"))
