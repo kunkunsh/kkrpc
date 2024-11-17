16f50ec2f6465ed3518d03872ab0b0e9358622b3
Huakun Shen <huakun.shen@huakunshen.com>
2024-11-17 06:51:39 -0500

    Typedoc Docs (#5)
    * docs: add typedoc docs
    
    * update README
    
    * ci: add docs branch
    
    * docs: add another typedoc entrypoint
    
    * ci: bypass deno error in cloudflare pages
    
    * ci: fix cloudflare pages

--- packages/kkrpc/scripts/prepare.ts
+++ packages/kkrpc/scripts/prepare.ts
@@ -1,6 +1,12 @@
 import { $ } from "bun"
 
-// const denoTypes = await $`deno types`.text()
-// // filter out the line with no-default-lib
-// const denoTypesFiltered = denoTypes.split("\n").filter((line) => !line.includes("no-default-lib"))
-// await Bun.write("./deno.d.ts", denoTypesFiltered.join("\n"))
+// check if CF_PAGES is set to 1
+const isCFPages = process.env.CF_PAGES === "1"
+if (isCFPages) {
+	process.exit(0)
+}
+
+const denoTypes = await $`deno types`.text()
+// filter out the line with no-default-lib
+const denoTypesFiltered = denoTypes.split("\n").filter((line) => !line.includes("no-default-lib"))
+await Bun.write("./deno.d.ts", denoTypesFiltered.join("\n"))
