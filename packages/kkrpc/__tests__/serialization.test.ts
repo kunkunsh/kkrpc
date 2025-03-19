e6bff1e62a7a8ac0fe215941142208c861e56d82
Huakun Shen <huakun.shen@huakunshen.com>
2025-03-19 00:47:44 -0400

    fix: unit test

--- packages/kkrpc/__tests__/serialization.test.ts
+++ packages/kkrpc/__tests__/serialization.test.ts
@@ -1,12 +1,6 @@
 import { describe, expect, test } from "bun:test"
 import superjson from "superjson"
-import {
-	deserializeMessage,
-	deserializeResponse,
-	serializeMessage,
-	serializeResponse,
-	type Message
-} from "../src/serialization.ts"
+import { deserializeMessage, serializeMessage, type Message } from "../src/serialization.ts"
 
 describe("Serializer", () => {
 	test("should serialize and deserialize a message", async () => {
@@ -24,14 +18,14 @@ describe("Serializer", () => {
 		expect(deserialized).toEqual(expectedMessage as any)
 	})
 
-	test("should serialize and deserialize a response", async () => {
-		const response = {
-			id: 1
-		}
-		const serializedResponse = serializeResponse(response as any)
-		const deserializedResponse = await deserializeResponse(serializedResponse)
-		expect(deserializedResponse).toEqual(response as any)
-	})
+	// test("should serialize and deserialize a response", async () => {
+	// 	const response = {
+	// 		id: 1
+	// 	}
+	// 	const serializedResponse = serializeResponse(response as any)
+	// 	const deserializedResponse = await deserializeResponse(serializedResponse)
+	// 	expect(deserializedResponse).toEqual(response as any)
+	// })
 
 	test("should serialize and deserialize a message with json version", async () => {
 		const message: Message = {
