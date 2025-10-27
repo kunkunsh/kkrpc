ba26e98905803e3ee6fc935b0b87253b54033d05
Huakun Shen <huakun.shen@huakunshen.com>
2025-10-27 23:47:58 +0800

    ci(deno): run deno tests in separate ci step

--- packages/kkrpc/scripts/test.ts
+++ packages/kkrpc/scripts/test.ts
@@ -1,6 +1,5 @@
 import { $ } from "bun"
-
-await $`deno test -R __deno_tests__`
+// await $`deno test -R __deno_tests__`
 const buildOutput = await Bun.build({
 	entrypoints: ["__tests__/scripts/node-api.js"],
 	outdir: "__tests__/scripts",
